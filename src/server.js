446ca553c82e3a5f6390cfd2bfb10b8ea449e879
Tautvydas Tunkis <ttunkis@gmail.com>
2022-05-05 14:41:03 +0300

    init

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
